74c50ff5cca57bd6bbf323efc01aa5e9edf4f5cf
Kyle Andrews <codingwithkyle@gmail.com>
2021-08-19 07:24:13 -0400

    Create ws.ts

---
+++ src/controllers/ws.ts
@@ -0,0 +1,36 @@
+import cc from "./control-center";
+
+let socket;
+let connected = false;
+
+function reconnect(){
+    return;
+    socket = new WebSocket('ws://localhost:5004');
+    socket.addEventListener('message', (event) => {
+        try {
+            const op = JSON.parse(event.data);
+            cc.perform(op, true);
+        } catch (e) {
+            console.error(e, event);
+        }
+    });
+    socket.addEventListener("close", () => {
+        disconnect();
+    });
+    socket.addEventListener("open", () => {
+        connected = true;
+        cc.sync();
+    });
+}
+reconnect();
+
+function disconnect(){
+    if (connected){
+        console.warn("Network connection has been lost.");
+        connected = false;
+    }
+    setTimeout(() => {
+        reconnect();
+    }, 5000);
+}
+export { connected, disconnect };
