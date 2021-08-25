8e08f43914f850ea88b1f93a696b079d4354627d
Kyle Andrews <codingwithkyle@gmail.com>
2021-08-25 07:31:13 -0400

    Update ws.ts

--- src/controllers/ws.ts
+++ src/controllers/ws.ts
@@ -7,7 +7,7 @@ function connect(){
     if (connected){
         return;
     }
-    socket = new WebSocket('wss://schemasketcher.com:8443');
+    socket = new WebSocket('wss://3.22.114.84:5004');
     socket.addEventListener('message', (event) => {
         try {
             const op = JSON.parse(event.data);
