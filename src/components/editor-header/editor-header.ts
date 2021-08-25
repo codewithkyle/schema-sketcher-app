8d17b780fb31f1211410da18f961324a1a8b2deb
Kyle Andrews <codingwithkyle@gmail.com>
2021-08-25 07:16:13 -0400

    Update editor-header.ts

--- src/components/editor-header/editor-header.ts
+++ src/components/editor-header/editor-header.ts
@@ -3,6 +3,7 @@ import { render, html } from "lit-html";
 import SettingsModal from "~components/settings-modal/settings-modal";
 import { css, mount } from "~controllers/env";
 import diagramController from "~controllers/diagram-controller";
+import { connect } from "~controllers/ws";
 
 interface IEditorHeader {
     name: string,
@@ -36,6 +37,10 @@ export default class EditorHeader extends SuperComponent<IEditorHeader>{
         const modal = new SettingsModal();
         document.body.appendChild(modal);
     }
+    
+    private openCollabModal:EventListener = (e:Event) => {
+        connect();
+    }
 
     override render(){
         const view = html`
@@ -50,7 +55,7 @@ export default class EditorHeader extends SuperComponent<IEditorHeader>{
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" /></svg>
                     Share
                 </button>
-                <button class="bttn ml-0.5" kind="text" color="white" icon="left">
+                <button @click=${this.openCollabModal} class="bttn ml-0.5" kind="text" color="white" icon="left">
                     <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M304 192c-41.7 0-76 32-79.7 72.8-25.2-1.3-61.6-7.9-88.8-31.7-20.3-17.8-32.8-43-37.5-75.1 35.5-8.2 62-40 62-77.9 0-44.2-35.8-80-80-80S0 35.8 0 80c0 38.7 27.5 71 64 78.4v195.2C27.5 361 0 393.3 0 432c0 44.2 35.8 80 80 80s80-35.8 80-80c0-38.7-27.5-71-64-78.4V237.4c5.5 7.2 11.7 13.9 18.6 19.9C151 289 197.9 296.1 228 297c10.5 31.9 40.5 55 76 55 44.2 0 80-35.8 80-80s-35.8-80-80-80zM32 80c0-26.5 21.5-48 48-48s48 21.5 48 48-21.5 48-48 48-48-21.5-48-48zm96 352c0 26.5-21.5 48-48 48s-48-21.5-48-48 21.5-48 48-48 48 21.5 48 48zm176-112c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48z"></path></svg>
                     Collaborate
                 </button>
