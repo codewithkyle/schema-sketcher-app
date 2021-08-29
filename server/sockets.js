85f5dc9bad97307cf5695a4ddde2bc034c099bee
Kyle Andrews <codingwithkyle@gmail.com>
2021-08-29 08:55:17 -0400

    Fixing socket disconnect issue

--- server/sockets.js
+++ server/sockets.js
@@ -3,7 +3,9 @@ const sockets = [];
 function addSocket(socket){
     sockets.push(socket);
     console.log(`Socket ${socket.id} connected`);
-    socket.on("disconnect", handleDisconnect(socket.id));
+    socket.on("disconnect", ()=>{
+        handleDisconnect(socket.id);
+    });
 }
 
 function handleDisconnect(id){
