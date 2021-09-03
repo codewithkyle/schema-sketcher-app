31bb4ee8af34351d43b4b0f26a757ecc58c235c8
Kyle Andrews <codingwithkyle@gmail.com>
2021-09-03 07:10:58 -0400

    Update session-controller.ts

--- src/controllers/session-controller.ts
+++ src/controllers/session-controller.ts
@@ -1,3 +1,5 @@
+import { connect, send } from "~controllers/ws";
+
 class SessionController {
     public room: string;
     public diagram: string;
@@ -9,10 +11,28 @@ class SessionController {
         this.requiresPassword = false;
     }
 
-    public createSession(room, diagram, requiresPassword){
+    public set(room, diagram, requiresPassword){
         this.room = room;
         this.diagram = diagram;
         this.requiresPassword = requiresPassword;
+    }
+    
+    public create(){
+        if (!this.room){
+            // TODO: add user modal to get the following details
+            await connect();
+            send("create-room", {
+                password: "",
+                allowAnon: true,
+                diagramID: diagramController.ID,
+            });   
+        }
+    }
+    
+    public leave(){
+        this.diagram = null;
+        this.room = null;
+        this.requiresPassword = false;
     }
 
     public getURL(){
