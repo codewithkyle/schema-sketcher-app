8345cf082d594550100f939a53241779315c0b54
Kyle Andrews <codingwithkyle@gmail.com>
2021-09-28 08:08:11 -0400

    Fixing AABB center x offset issues

--- src/components/canvas-component/canvas-component.ts
+++ src/components/canvas-component/canvas-component.ts
@@ -275,8 +275,9 @@ export default class CanvasComponent extends HTMLElement{
                 this.ctx.lineTo(endX, endY);
 
                 this.hitCTX.fillStyle = "red";
-                this.hitCTX.fillRect(startX, startY - 8, Math.abs(startX - centerX) + offsetX, 16);
-                this.hitCTX.fillRect(centerX + offsetX, endY - 8, Math.abs(endX - centerX), 16);
+                this.hitCTX.fillRect(startX, startY - 8, Math.abs(startX - centerX), 16);
+                this.hitCTX.fillRect(centerX, endY - 8, Math.abs(endX - centerX), 16);
+                
             }
         }
         else if (startSide === "bottom" && endSide === "top"){
