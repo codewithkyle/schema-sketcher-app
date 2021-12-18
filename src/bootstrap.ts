b9a8ad942bb8dc63dee2dab0bc1809a09a2c3e28
Kyle Andrews <codingwithkyle@gmail.com>
2021-12-18 18:06:04 -0500

    Switched to CDN version of JSQL workers

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -3,19 +3,17 @@ import db from "@codewithkyle/jsql";
 (async () => {
     // @ts-ignore
     const { env } = await import("/config.js");
-    if (env === "production"){
+    if (env === "production") {
         await navigator.serviceWorker.register(`${location.origin}/service-worker.js`);
     }
 
     await db.start({
         schema: `${location.origin}/static/schema.json`,
-        dbWorker: `${location.origin}/static/jsql.worker.js`,
-        streamWorker: `${location.origin}/static/stream.worker.js`,
     });
 
     // @ts-ignore
     const items = await db.query("SELECT name FROM icons");
-    if (items.length !== 2681){
+    if (items.length !== 2681) {
         // @ts-ignore
         db.ingest(`${location.origin}/static/icons.ndjson`, "icons");
     }
