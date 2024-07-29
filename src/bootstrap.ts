daae23a586bdf6ecb27a514a54d8453a4fa6a339
Kyle Andrews <codingwithkyle@gmail.com>
2024-07-28 21:51:40 -0400

    Switched service worker

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,6 +1,30 @@
 (async () => {
     if (window.location.hostname !== "localhost" && "serviceWorker" in navigator) {
-        await navigator.serviceWorker.register(`${location.origin}/service-worker.js`);
+        await new Promise<void>(resolve => {
+            navigator.serviceWorker.register("/service-worker.js", { scope: "/" });
+            navigator.serviceWorker.addEventListener("message", (e) => {
+                localStorage.setItem("version", e.data);
+            });
+            navigator.serviceWorker.ready.then(async (registration) => {
+                try {
+                    await import("/static/service-worker-assets.js?t=" + Date.now());
+                    // @ts-expect-error
+                    if (self.manifest.version !== localStorage.getItem("version")) {
+                        localStorage.removeItem("version");
+                    } else {
+                        // @ts-expect-error
+                        delete self.manifest.assets;
+                    }
+                    // @ts-expect-error
+                    registration.active.postMessage(self.manifest);
+                } catch (e) {
+                    registration.active.postMessage({
+                        version: localStorage.getItem("version") || "init",
+                    });
+                }
+                resolve();
+            });
+        });
     }
 
     //@ts-ignore
